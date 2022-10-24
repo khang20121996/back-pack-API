84a1b8959810ec6665436da2cf2e648f39c77fa7
Khang Nguyen <nguyentruongkhang.1509@gmail.com>
2022-10-24 17:38:33 +0700

    - Add query-string Lib - Custom output res with pagination

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const jsonServer = require('json-server');
+const queryString = require('query-string');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
@@ -17,10 +18,40 @@ server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
   if (req.method === 'POST') {
     req.body.createdAt = Date.now();
+    req.body.updateAt = Date.now();
+  } else if (req.method === 'PATCH') {
+    req.body.updateAt = Date.now();
   }
   // Continue to JSON Server router
   next();
 });
+
+router.render = (req, res) => {
+  // check GET with pagination
+  // if ys, custom output
+  const headers = res.getHeaders();
+
+  const totalCountHeader = headers['x-total-count'];
+  if (req.method === 'GET' && totalCountHeader) {
+    const queryParams = queryString.parse(req._parsedUrl.query);
+
+    const result = {
+      data: res.locals.data,
+      pagination: {
+        _page: Number.parseInt(queryParams._page) || 1,
+        _limit: Number.parseInt(queryParams._limit) || 10,
+        _totalRows: Number.parseInt(totalCountHeader),
+      },
+    };
+
+    return res.jsonp(result);
+  }
+
+  // otherwise, keep defaut behavior
+  res.jsonp({
+    body: res.locals.data,
+  });
+};
 
 // Use default router
 server.use(router);
